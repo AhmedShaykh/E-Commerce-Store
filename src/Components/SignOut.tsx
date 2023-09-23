e8c193b3006ec58031d8171c70625d02491c2a3b
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-23 23:22:33 +0500

    Minor Change

--- src/Components/SignOut.tsx
+++ src/Components/SignOut.tsx
@@ -1,12 +1,24 @@
 "use client";
 import { signOut } from "next-auth/react";
 import { FiLogOut } from "react-icons/fi";
+import { useRouter } from "next/navigation";
 
 const SignOut = () => {
+
+    const { refresh } = useRouter();
+
+    const logOut = () => {
+
+        signOut();
+
+        refresh();
+
+    };
+
     return (
         <div
             className="headerDiv px-2 gap-x-1 cursor-pointer"
-            onClick={() => signOut()}
+            onClick={logOut}
         >
             <FiLogOut className="text-2xl" />
 
