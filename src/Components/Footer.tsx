b3fc7773c3b8c613deae686e124ec28fd2b16cd8
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-23 23:24:11 +0500

    Minor Change

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -26,41 +26,36 @@ const Footer = () => {
                     </p>
 
                     <div className="flex items-center gap-x-4">
-                        <a href="" target="_blank">
-                            <span
-                                className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-red-600 hover:text-white cursor-pointer duration-200"
-                            >
-                                <BsYoutube />
-                            </span>
-                        </a>
-                        <a href="" target="_blank">
-                            <span
-                                className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-black hover:text-white cursor-pointer duration-200"
-                            >
-                                <BsGithub />
-                            </span>
-                        </a>
-                        <a href="" target="_blank">
-                            <span
-                                className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-cyan-700 hover:text-white cursor-pointer duration-200"
-                            >
-                                <BsLinkedin />
-                            </span>
-                        </a>
-                        <a href="" target="_blank">
-                            <span
-                                className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-blue-800 hover:text-white cursor-pointer duration-200"
-                            >
-                                <BsFacebook />
-                            </span>
-                        </a>
-                        <a href="" target="_blank">
-                            <span
-                                className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-black hover:text-blue-800 cursor-pointer duration-200"
-                            >
-                                <BsTwitter />
-                            </span>
-                        </a>
+                        <span
+                            className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-red-600 hover:text-white cursor-pointer duration-200"
+                        >
+                            <BsYoutube />
+                        </span>
+
+                        <span
+                            className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-black hover:text-white cursor-pointer duration-200"
+                        >
+                            <BsGithub />
+                        </span>
+
+                        <span
+                            className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-cyan-700 hover:text-white cursor-pointer duration-200"
+                        >
+                            <BsLinkedin />
+                        </span>
+
+                        <span
+                            className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-blue-800 hover:text-white cursor-pointer duration-200"
+                        >
+                            <BsFacebook />
+                        </span>
+
+                        <span
+                            className="bg-slate-100 p-1 text-darkText inline-flex items-center justify-center rounded-md text-lg hover:bg-black hover:text-blue-800 cursor-pointer duration-200"
+                        >
+                            <BsTwitter />
+                        </span>
+
                     </div>
                 </div>
 
