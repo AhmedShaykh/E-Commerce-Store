ce43d0e64624a0f252063566dd76f0f7998e5369
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-24 23:04:45 +0500

    Add Stripe

--- src/Components/PaymentForm.tsx
+++ src/Components/PaymentForm.tsx
@@ -3,12 +3,19 @@ import React, { useEffect, useState } from "react";
 import { ProductsType, StateProps } from "../../Types";
 import FormattedPrice from "./FormattedPrice";
 import { useDispatch, useSelector } from "react-redux";
+import { loadStripe } from "@stripe/stripe-js";
+import { useSession } from "next-auth/react";
+import { resetCart, saveOrder } from "@/redux/cartSlice";
 
 const PaymentForm = () => {
 
     const [totalAmt, setTotalAmt] = useState<number>(0);
 
     const { productData, userInfo } = useSelector((state: StateProps) => state?.cart);
+
+    const { data: session } = useSession();
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
 
@@ -25,7 +32,40 @@ const PaymentForm = () => {
 
     }, [productData]);
 
-    const dispatch = useDispatch();
+    const stripePromise = loadStripe(
+        process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+    );
+
+    const handleCheckout = async () => {
+
+        const stripe = await stripePromise;
+
+        const response = await fetch("/api/checkout", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                items: productData,
+                email: session?.user?.email
+            })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+
+            await dispatch(saveOrder({ order: productData, id: data.id }));
+
+            stripe?.redirectToCheckout({ sessionId: data.id });
+
+            dispatch(resetCart());
+
+        } else {
+
+            throw new Error("Failed To Create Stripe Payment");
+
+        }
+
+    };
 
     return (
         <div className="w-full bg-white p-4">
@@ -61,14 +101,15 @@ const PaymentForm = () => {
             {userInfo ? (
                 <div className="flex justify-center">
                     <button
-                        className="mx-auto bg-black text-slate-100 mt-4 py-3 px-6 hover:bg-orange-950 cursor-pointer duration-200"
+                        className="mx-auto bg-black text-slate-100 mt-4 py-3 px-6 hover:bg-orange-600 cursor-pointer duration-200"
+                        onClick={handleCheckout}
                     >
                         Proceed To Checkout
                     </button>
                 </div>
             ) : (
                 <div className="flex flex-col items-center">
-                    <button className="mx-auto bg-black text-slate-100 mt-4 mb-2 py-3 px-6 hover:bg-orange-950 cursor-not-allowed duration-200">
+                    <button className="mx-auto bg-black text-slate-100 mt-4 mb-2 py-3 px-6 hover:bg-orange-600 cursor-not-allowed duration-200">
                         Proceed To Checkout
                     </button>
 
